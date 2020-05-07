b62db2cc8ce8423af2d5aa59cafa12130bd93b7a
Jofre <aejofre@gmail.com>
2020-05-07 00:10:45 -0500

    Juego de gato

---
+++ script.js
@@ -0,0 +1,63 @@
+var aa = {
+    valor: "?",
+}
+
+var aaValor = document.getElementById("aa-valor");
+aaValor.innerHTML = aa.valor;
+
+var ab = {
+    valor: "?",
+}
+
+var abValor = document.getElementById("ab-valor");
+abValor.innerHTML = ab.valor;
+
+var ac = {
+    valor: "?",
+}
+
+var acValor = document.getElementById("ac-valor");
+acValor.innerHTML = ac.valor;
+
+var ba = {
+    valor: "?",
+}
+
+var baValor = document.getElementById("ba-valor");
+baValor.innerHTML = ba.valor;
+
+var bb = {
+    valor: "?",
+}
+
+var bbValor = document.getElementById("bb-valor");
+bbValor.innerHTML = bb.valor;
+
+var bc = {
+    valor: "?",
+}
+
+var bcValor = document.getElementById("bc-valor");
+bcValor.innerHTML = bc.valor;
+
+var ca = {
+    valor: "?",
+}
+
+var caValor = document.getElementById("ca-valor");
+caValor.innerHTML = ca.valor;
+
+var cb = {
+    valor: "?",
+}
+
+var cbValor = document.getElementById("cb-valor");
+cbValor.innerHTML = cb.valor;
+
+var cc = {
+    valor: "?",
+}
+
+var ccValor = document.getElementById("cc-valor");
+ccValor.innerHTML = cc.valor;
+
